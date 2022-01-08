3cf7476af93c5615b027319c9154ecd91f37dc12
Edison Li <47466335+edison4354@users.noreply.github.com>
2022-01-07 16:07:13 -0800

    Setup Hero Section for homepage

---
+++ src/components/HeroSection/HeroElements.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const HeroContainer = styled.div`
+  background: #10122b;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+  height: 900px;
+  position: relative;
+  z-index: 1;
+`
+
+export const HeroBg = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+`
---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+  HeroContainer,
+  HeroBg
+} from './HeroElements'
+
+const HeroSection = () => {
+  return (
+    <HeroContainer>
+      <HeroBg>
+      </HeroBg> 
+    </HeroContainer>
+  )
+}
+
+export default HeroSection
